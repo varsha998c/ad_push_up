d9567b07d8ec5121321ffebb1c1b1bee71d37184
@varsha.woodenclouds <varsha.woodenclouds@gmail.com>
2024-10-23 01:51:07 +0530

    four technologies task

---
+++ spotlight/Script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $(".items").slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
